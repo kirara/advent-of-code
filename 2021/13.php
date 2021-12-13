82c41ec5af96eaa056de14e7d381274cde2d73e0
Martin "Kirara" Vendl <vmarty89@gmail.com>
2021-12-13 16:42:40 +0100

    day 13

---
+++ 2021/13.php
@@ -0,0 +1,38 @@
+<?php
+  [$coords, $instructions] = explode("\n\n", trim(file_get_contents('13.txt')));
+
+  $coords = explode("\n", $coords);
+  foreach ($coords as $row) {
+    [$x, $y] = explode(',', $row);
+    $mx = max($mx ?? 0, $x);
+    $my = max($my ?? 0, $y);
+    $map[$y][$x] = true;
+  }
+  
+  preg_match_all('/([xy])=(\d+)/', $instructions, $folds);
+  $folds = array_map(null, ...(array_slice($folds, 1)));
+
+  foreach ($folds as [$axis, $pos]) {
+    if ($axis == 'y') {
+      $w = $my - $pos;
+      for ($y = $pos - $w; $y < $pos; $y++) {
+        for ($x = 0; $x <= $mx; $x++) {
+          if (!empty($map[$y][$x]) | !empty($map[$my-$y][$x])) $map[$y][$x] = true;
+          unset($map[$my-$y][$x]);
+        }
+      }
+      $my = $w-1;
+    } else {
+      $w = $mx - $pos;
+      for ($y = 0; $y <= $my; $y++) {
+        for ($x = $pos - $w; $x < $pos; $x++) {
+          if (!empty($map[$y][$x]) | !empty($map[$y][$mx-$x])) $map[$y][$x] = true;
+          unset($map[$y][$mx-$x]);
+        }
+      }
+      $mx = $w-1;
+    }
+    break;
+  }
+
+  echo array_sum(array_merge(...$map)) . "\n";
